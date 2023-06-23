5b42b24f9e3dbb7bd5b2903ae084aa68238fc149
asmita <asmitamavani804@gmail.com>
2023-06-23 19:50:59 +0530

    first commit

--- app/src/main/java/com/example/experiment/MainActivity.java
+++ app/src/main/java/com/example/experiment/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     String str="";
     TimePickerDialog timePickerDialog;
+    private String am_pm="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,11 +91,19 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick(View v) {
                Calendar calendar=Calendar.getInstance();
-               timePickerDialog=new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
 
+               timePickerDialog=new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
                    @Override
                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                       binding.ans.setText(""+hourOfDay+":"+minute);
+                       calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                       calendar.set(Calendar.MINUTE, minute);
+
+                       if (calendar.get(Calendar.AM_PM) == Calendar.AM)
+                           am_pm = "AM";
+                       else if (calendar.get(Calendar.AM_PM) == Calendar.PM)
+                           am_pm = "PM";
+                       String strHrsToShow = (calendar.get(Calendar.HOUR) == 0) ?"12":Integer.toString(calendar.get(Calendar.HOUR) );
+                       binding.ans.setText(""+strHrsToShow+":"+minute+" "+am_pm);
                    }
                },calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false);
                timePickerDialog.show();
